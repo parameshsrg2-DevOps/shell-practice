464a45e88ab9ab3b24df8cae77add75ca25f02e3
Sivakumar Reddy M <info@joindevops.com>
2026-02-03 08:47:55 +0530

    actions

--- 23-delete-old-logs.sh
+++ 23-delete-old-logs.sh
@@ -14,9 +14,9 @@ if [ ! -d $LOGS_DIR ]; then
 fi
 
 FILES_TO_DELETE=$(find $LOGS_DIR -name "*.log" -mtime +14)
-echo "$FILES_TO_DELETE"
+# echo "$FILES_TO_DELETE"
 
 while IFS= read -r filepath; do
   # Process each line here
-  echo "$filepath"
+  echo "Deleting file: $filepath"
 done <<< $FILES_TO_DELETE
